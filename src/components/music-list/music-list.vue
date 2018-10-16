604fd1b07847ec96447f590b5edf64fab579706f
ybq.win10 <583497794@qq.com>
2018-10-16 23:16:31 +0800

    未完成 auto-prefix

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <!-- 模糊层 -->
-      <div class="filter"></div>
+      <div class="filter" ref="filter"></div>
     </div>
+    <div class="bg-layer" ref="layer"></div>
     <!-- 传入data可以及时的更新scroll，watch data -->
-    <scroll :data="songs" class="list" ref="list">
+    <scroll :data="songs" class="list" ref="list" :probe-type="probeType" :listen-scroll="listenScroll" @scroll="scroll">
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
@@ -20,8 +27,17 @@
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom'
+const RESERVED_HIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 export default {
   name: 'music-list',
+  data () {
+    return {
+      scrollY: 0
+    }
+  },
   props: {
     bgImage: {
       type: String,
@@ -40,18 +56,66 @@ export default {
     Scroll,
     SongList
   },
-  data () {
-    return {
-
-    }
-  },
   computed: {
     bgStyle () {
       return `background-image: url(${this.bgImage})`
     }
   },
+  created() {
+    this.probeType = 3
+    this.listenScroll = true
+  },
   mounted() {
-    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.minTranslateY = -this.imageHeight + RESERVED_HIGHT
+    this.$refs.list.$el.style.top = `${this.imageHeight}px`
+  },
+  methods: {
+    scroll (pos) {
+      this.scrollY = pos.y
+    },
+    back () {
+      this.$router.push('/singer')
+    }
+  },
+  watch: {
+    scrollY (newY) {
+      let translateY = Math.max(this.minTranslateY, newY)
+      let zIndex = 0
+      let scale = 1
+      let blur = 0
+      console.log(transform)
+      console.log(backdrop)
+      this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px, 0)`
+      this.$refs.layer.style['webkit-transform'] = `translate3d(0,${translateY}px, 0)`
+      // this.$refs.layer.style[transform] = `translate3d(0,${translateY}px, 0)`
+      console.log(this.$refs.layer.style[transform])
+      const percent = Math.abs(newY / this.imageHeight)
+      if (newY > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      } else {
+        blur = Math.min(20 * percent, 20)
+      }
+      this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
+      this.$refs.filter.style['webkit-backdrop-filter'] = `blur(${blur}px)`
+      // this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      // 控制图片z-index，置于歌单之上
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = `70%`
+        this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style['transform'] = `scale(${scale})`
+      this.$refs.bgImage.style['webkit-transform'] = `scale(${scale})`
+      // this.$refs.layer.style[transform] = `scale(${scale})`
+    }
   }
 }
 </script>
@@ -96,6 +160,33 @@ export default {
       padding-top 70%
       transform-origin top
       background-size cover
+      .play-wrapper
+        position absolute
+        bottom 20px
+        z-index 50
+        width 100%
+        // margin-left 50%
+        // left -50%
+        // border 1px solid $color-theme
+        // border-radius 10px
+        .play
+          box-sizing border-box
+          width 135px
+          padding 7px 0
+          margin 0 auto
+          text-align center
+          border 1px solid $color-theme
+          border-radius 100px
+          font-size 0
+          .icon-play
+            display inline-block
+            vertical-align middle
+            margin-right 6px
+            font-size $font-size-medium-x
+          .text
+            display inline-block
+            vertical-align middle
+            font-size $font-size-small
       .filter
         position absolute
         top 0
@@ -103,15 +194,18 @@ export default {
         width 100%
         height 100%
         background rgba(7, 17, 27, 0.4)
-    .list
-      position absolute
+    .bg-layer
+      position relative
+      height 100%
       width 100%
-      bottom 0
+      background $color-background
+    .list
+      position fixed
+      width 100%
+      bottom  0
       left 0
-      overflow hidden
+      // overflow hidden
       .song-list-wrapper
         position absolute
         width 100%
-        top 0
-        left 0
 </style>
